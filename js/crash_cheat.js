6dc62f45bc9255d78562fe20bd3ec1b3157c31dc
dinggan <dinggan@whu.edu.cn>
2020-03-16 22:42:49 +0800

    Site updated: 2020-03-16 22:42:22

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -3,12 +3,12 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/images/logo.ico");
+         $('[rel="icon"]').attr('href', "/images/tag_larger.png");
          document.title = '╭(°A°`)╮ 哎呀页面崩溃啦 ~';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/images/logo.ico");
+         $('[rel="icon"]').attr('href', "/images/tag_larger.png");
          document.title = '(ฅ>ω<*ฅ) 噫又好了哎~' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
