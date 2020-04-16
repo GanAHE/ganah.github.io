31496a1df28e7433e890d3b92da1d8b27c863801
dinggan <dinggan@whu.edu.cn>
2020-04-16 18:25:34 +0800

    Site updated: 2020-04-16 18:25:00

--- live2d/autoload.js
+++ live2d/autoload.js
@@ -33,9 +33,9 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-                                                //apiPath: "https://ganah.github.io/live2d_api/",
-			apiPath: "https://live2d.fghrsh.net/api/",
-			//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
+            //apiPath: "https://ganah.github.io/live2d_api/",
+			//apiPath: "https://live2d.fghrsh.net/api/",
+			cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
 		});
 	});
 }
